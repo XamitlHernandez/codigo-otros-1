df7582a10b9f07cfb6d8be43b26f5284123002f3
XamitlHernandez <xamitlhernandez@gmail.com>
2024-11-29 11:19:37 -0600

    Ejercicio 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,29 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name'); // le hace falta un punto para trabajar con la clase
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) { // hace falta la palabra reservada async ya que esta va con el await
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  const response = await fetch(`${usersEndpoint}/${username}`)// Mandamos a traer la informacion pero que hacemos con la informacion
+  .then((response)=>{ 
+    return response.json(); // transformamos la informacion en JSON para trabajar con ella
+  }).then((data)=>{ //Guardamos la informacion para trabajar con ella
+    console.log(data); // Incorporamos este console log para que nos de la informacion
+    localStorage.setItem("name",data.name);
+    localStorage.setItem("blog",data.blog);
+    localStorage.setItem("location",data.location);
+  })
+   // Mandamos a traer la informacion y mandamos cambiar los datos en el html
+  $n.textContent = localStorage.getItem("name");
+  $b.textContent = localStorage.getItem("blog");
+  $l.textContent = localStorage.getItem("location");
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`// no estaba bien nombrada la variable
 }
-
 displayUser('stolinski').catch(handleError);
